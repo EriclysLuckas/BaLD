5a79fc2938c13df728d9a73b6aa8f9cf51780bf2
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-09-12 17:24:31 -0300

    at

--- BaLD/src/components/TypeLashs/TypeLashs.jsx
+++ BaLD/src/components/TypeLashs/TypeLashs.jsx
@@ -1,20 +1,23 @@
 import styleTypeLashs from "../TypeLashs/TypeLashs.module.css";
-
-// import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
 export default function TypeLashs() {
-
-
-
-
-
-
   return (
     <section className={styleTypeLashs.SectionTypeLashs}>
       <h1>Tipos de Designs</h1>
 
       <div className={styleTypeLashs.TypeLash}>
-      
+        <Swiper>
+          <SwiperSlide >
+            <p>Teste 1</p>
+            <figure> <img src="../img/ciliosBanner.jpg" alt="teste" /></figure>
+          </SwiperSlide>
+          <SwiperSlide >
+            <p>Teste 2</p>
+            <figure> <img src="../img/ciliosBanner.jpg" alt="teste" /></figure>
+          </SwiperSlide>
+        </Swiper>
+
       </div>
 
 
