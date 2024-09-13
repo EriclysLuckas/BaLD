4ae4751f92976a466cb48bc336635fa6b4ecf45b
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-09-13 17:27:15 -0300

    carrossel

--- BaLD/src/components/TypeLashs/TypeLashs.jsx
+++ BaLD/src/components/TypeLashs/TypeLashs.jsx
@@ -1,20 +1,30 @@
 import styleTypeLashs from "../TypeLashs/TypeLashs.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
+
+// Import Swiper styles
+import 'swiper/css/autoplay'; 
+
+
 export default function TypeLashs() {
   return (
     <section className={styleTypeLashs.SectionTypeLashs}>
       <h1>Tipos de Designs</h1>
 
       <div className={styleTypeLashs.TypeLash}>
-        <Swiper>
+      <Swiper  
+        loop={true} // Ativa o loop infinito
+        modules={[  ]}
+        slidesPerView={1}
+        >
+
           <SwiperSlide >
             <p>Teste 1</p>
-            <figure> <img src="../img/ciliosBanner.jpg" alt="teste" /></figure>
+            <figure className = {styleTypeLashs.figureswiper}> <img src="../img/ciliosBanner.jpg" alt="teste" /></figure>
           </SwiperSlide>
           <SwiperSlide >
             <p>Teste 2</p>
-            <figure> <img src="../img/ciliosBanner.jpg" alt="teste" /></figure>
+            <figure className = {styleTypeLashs.figureswiper}> <img src="../img/Typelash.png" alt="teste" /></figure>
           </SwiperSlide>
         </Swiper>
 
