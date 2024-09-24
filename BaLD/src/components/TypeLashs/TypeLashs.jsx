7bc5310850025b70c100c42aa1fb90dbc1eb6c02
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-09-24 15:53:53 -0300

    att

--- BaLD/src/components/TypeLashs/TypeLashs.jsx
+++ BaLD/src/components/TypeLashs/TypeLashs.jsx
@@ -1,13 +1,18 @@
 import styleTypeLashs from "../TypeLashs/TypeLashs.module.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-// import { Autoplay } from "swiper";
+import 'swiper/css/bundle';
+
 import TypesLashsJson from "../TypeLashs/TypeLashs.json";
+
+//modules swiper
+import { EffectFade, Autoplay } from 'swiper/modules';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/autoplay';
 
 
 export default function TypeLashs() {
 
-  
+
   return (
     <section className={styleTypeLashs.SectionTypeLashs}>
       <h1>Tipos de Designs</h1>
@@ -15,37 +20,37 @@ export default function TypeLashs() {
       <div className={styleTypeLashs.TypeLash}>
 
 
-    
-      <Swiper  className = {styleTypeLashs.swiper}
 
-        loop={true} 
-        // modules={[ Autoplay ]}
-        // autoplay={{
-        //   delay: 2500, 
-        //   disableOnInteraction: false, 
-        // }}
-        slidesPerView={1}
-        spaceBetween={50}
+        <Swiper className={styleTypeLashs.swiper}
+          modules={[EffectFade, Autoplay]}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          effect="fade"
+          loop={true}
+          slidesPerView={1}
+          spaceBetween={50}
 
         >
 
-          {TypesLashsJson.map((item) =>(
+          {TypesLashsJson.map((item) => (
             <SwiperSlide key={item.id}>
-              <p className = {styleTypeLashs.titleSlide}>{item.name} <img src={item.min} alt="" /></p>
-              <figure className = {styleTypeLashs.figureSlider}>           
-              <img src={item.img} alt ="slide" />
-              <img src={item.img} alt ="slide" className = {styleTypeLashs.imgBackground} />
-
+              <div className={styleTypeLashs.slideBackground} />
+              <p className={styleTypeLashs.titleSlide}>{item.name} <img src={item.min} alt="" /></p>
+              <figure className={styleTypeLashs.figureSlider}>
+                <img src={item.img} alt="slide" />
+                <img src={item.img} alt="slide" className={styleTypeLashs.imgBackground} />
               </figure>
             </SwiperSlide>
           ))}
-        </Swiper> 
-        
-        
+        </Swiper>
+
+
 
       </div>
 
-      
+
 
     </section>
 
