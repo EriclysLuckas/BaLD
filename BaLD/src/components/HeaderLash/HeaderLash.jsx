057db898cdfe52e8a464850d37ac7273470d9eea
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-11-04 17:04:55 -0300

    att

--- BaLD/src/components/HeaderLash/HeaderLash.jsx
+++ BaLD/src/components/HeaderLash/HeaderLash.jsx
@@ -5,18 +5,24 @@ export default function HeaderLash() {
 const [isOpen,setIsOpen] = useState(false)
 
 
-const handleOpenMenu () => {
-  setIsOpen = (true)
+const handleOpenMenu = () => {
+  setIsOpen(prevState=> !prevState)
+  console.log('teste')
 }
 
   return (
       <section className={styleHeader.HeaderLash} >
         <div className={styleHeader.contentHeaderLash}>
-          <div className={styleHeader.hambMenu}>  <IoMenu /></div>
+          <div className={styleHeader.hambMenu}>  <IoMenu onClick={handleOpenMenu}/></div>
           <div className={styleHeader.logoLash}> <figure> <img src="../img/logo.png" alt="" /></figure> </div>
         </div>
-        
-        
+          {(isOpen)&&(
+            <div className = {styleHeader.menuMobile}>
+              <div className = {styleHeader.contentMenu}>
+                <span>teste</span>
+              </div>
+            </div>
+          )}
       </section>
 
 
