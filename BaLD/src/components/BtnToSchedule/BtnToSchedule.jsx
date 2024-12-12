def3b1f7812dc1e7e7acc9b48887101a6f33a620
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-12-12 16:31:40 -0300

    Update BtnToSchedule.jsx

--- BaLD/src/components/BtnToSchedule/BtnToSchedule.jsx
+++ BaLD/src/components/BtnToSchedule/BtnToSchedule.jsx
@@ -1,11 +1,36 @@
 import styleBtnToSchedule from "../BtnToSchedule/BtnToSchedule.module.css"
-
+import { useState } from "react";
 
 export default function BtnToSchedule() {
-  
+
+const [formAgendamento, setFormAgendamento] = useState(false)
+
+
+const toggleBtnFormAgendamento = () => {
+  setFormAgendamento((prevState )=> !prevState)
+
+ 
+}
+
+
+
 
   return(
-    <button className = {styleBtnToSchedule.BtnToSchedule}>Agendar</button>
+    <>
+    <button className = {styleBtnToSchedule.BtnToSchedule} onClick ={toggleBtnFormAgendamento}>Agendar</button>
+   
+    {formAgendamento && (
+      <div className ={styleBtnToSchedule.ModalAgendamento}>
+          
+        
 
+
+      </div>
+
+
+
+    )} 
+    </>
+    
   )
 }
