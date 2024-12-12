85db57b8a17038a5cb67332ecbd12f64f6c13b4a
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-12-12 17:22:42 -0300

    inicio do form agendamento

--- BaLD/src/components/BtnToSchedule/BtnToSchedule.jsx
+++ BaLD/src/components/BtnToSchedule/BtnToSchedule.jsx
@@ -1,6 +1,8 @@
 import styleBtnToSchedule from "../BtnToSchedule/BtnToSchedule.module.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { IoCloseSharp  } from "react-icons/io5";
 
+import FormSite from "../FormSite/FormSite";
 export default function BtnToSchedule() {
 
 const [formAgendamento, setFormAgendamento] = useState(false)
@@ -8,11 +10,22 @@ const [formAgendamento, setFormAgendamento] = useState(false)
 
 const toggleBtnFormAgendamento = () => {
   setFormAgendamento((prevState )=> !prevState)
-
- 
+}
+const toggleBtnFormAgendamentoClose = () => {
+  setFormAgendamento(false)
 }
 
 
+useEffect(() => {
+  if (formAgendamento) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+return () =>{
+  document.body.style.overflow = 'auto'
+};
+}, [formAgendamento]);
 
 
   return(
@@ -21,8 +34,13 @@ const toggleBtnFormAgendamento = () => {
    
     {formAgendamento && (
       <div className ={styleBtnToSchedule.ModalAgendamento}>
-          
-        
+
+        <div className = {styleBtnToSchedule.contentModalAgendamento}>
+        <IoCloseSharp onClick ={toggleBtnFormAgendamentoClose}  className ={styleBtnToSchedule.closeModalAgendamento}/>
+
+          <FormSite />
+
+        </div>
 
 
       </div>
